45851ecb1be3f0a5a057aaad9694e63271a6e9c1
newnakashima <newnakashima2014@gmail.com>
2018-07-24 01:00:21 +0900

    なぜかテスト通らない

--- test.js
+++ test.js
@@ -6,6 +6,7 @@ function test_print_text()
     const child = spawn('node', ['./app.js']);
     child.stdout.on('data', function (chunk) {
         const actual = String.fromCharCode.apply(null, new Uint16Array(chunk));
+        console.log(actual);
         assert.strictEqual(actual, 'fugafuga\n');
         console.log('test OK');
     });
